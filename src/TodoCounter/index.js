b880c890af7a196d5dd76d798ab24da31505c390
cafalco96 <carlossfalco96@gmail.com>
2023-03-03 17:04:49 -0500

    fix counter values

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -5,7 +5,7 @@ function TodoCounter() {
   const { taskUserTotal, taskUserComplete } = React.useContext(TodoContext);
   return (
     <h2 className="to-do--item-status-title">
-      You has completed {taskUserComplete} of {taskUserTotal} Tasks
+      You have completed {taskUserComplete} of {taskUserTotal} Tasks
     </h2>
   );
 }
